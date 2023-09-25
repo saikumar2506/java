02f8e1a4b9b926433320adf8bd2ab00556d3d6d2
saikumar2506 <143429603+saikumar2506@users.noreply.github.com>
2023-09-25 11:27:20 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,9 @@
+public class A
+{
+    static {
+        System.out.println("Inside Static block");
+    }
+public static void main(String args[]){
+    System.out.println("Inside main method");
+    }
+}
